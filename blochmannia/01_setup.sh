9db99c14b3234f4e33e2d8a57ea2dbdb2aaea470
Bmeadows27 <55255460+Bmeadows27@users.noreply.github.com>
2022-02-25 12:23:06 -0600

    Update and rename name_of_file to 01_setup.sh

---
+++ blochmannia/01_setup.sh
@@ -0,0 +1,29 @@
+# Working directory
+/lustre/scratch/brameado/Blochmannia_Local_Adaptation
+
+# Directories
+mkdir 00_fastq
+mkdir 01_cleaned
+mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_filter_vcf_admixture
+mkdir 05_filter_vcf_heterozygosity
+mkdir 06_filter_vcf_GEA
+mkdir 07_LEA
+mkdir 08_BayeScEnv
+mkdir 10_align_script
+mkdir 12_filter_scripts
+mkdir 13_coverage_map
+
+#Transfer raw fastq files to 00_fastq
+#Create rename_files.txt file, popmap, and mtDNA files and add them to working directory
+
+#To rename files
+while read -r name1 name2; do
+	mv $name1 $name2
+done < rename_samples.txt
+
+#Copy reference genome to working directory
+cd /lustre/scratch/brameado/Blochmannia_Local_Adaptation
+cp /home/jmanthey/denovo_genomes/camp_sp_genome_filtered.fasta.fai .
