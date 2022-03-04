eea375f54d6030e319de5cdfbccee8dbb991ff6d
Bmeadows27 <55255460+Bmeadows27@users.noreply.github.com>
2022-03-04 11:16:17 -0600

    Update 02_index_ref.sh

--- blochmannia/02_index_ref.sh
+++ blochmannia/02_index_ref.sh
@@ -5,10 +5,10 @@ cd /lustre/scratch/brameado/Blochmannia_Local_Adaptation
 cp 2_modoc.fasta /lustre/scratch/brameado/Blochmannia_Local_Adaptation
 
 #Use Samtools faidx to index reference genome and create fasta.fai file
-samtools faidx camp_sp_genome_filtered.fasta 
+samtools faidx 2_modoc.fasta 
 
 #use bwa index to index database sequences in fasta format
-bwa index camp_sp_genome_filtered.fasta
+bwa index 2_modoc.fasta
 
 #use picard CreateSequenceDictionary to create a sequence dictionary for the reference sequences
-java -jar /home/jmanthey/picard.jar CreateSequenceDictionary R=02_modoc.fasta O=02_modoc.dict
+java -jar /home/jmanthey/picard.jar CreateSequenceDictionary R=2_modoc.fasta O=2_modoc.dict
