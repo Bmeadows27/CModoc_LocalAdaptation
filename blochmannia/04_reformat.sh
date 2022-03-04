216c8779aef3e5ed671f38d4ec983b1961532b83
Bmeadows27 <55255460+Bmeadows27@users.noreply.github.com>
2022-03-04 11:59:26 -0600

    Update 04_reformat.sh

--- blochmannia/04_reformat.sh
+++ blochmannia/04_reformat.sh
@@ -18,3 +18,8 @@ basename_array=$( head -n${SLURM_ARRAY_TASK_ID} ${workdir}/basenames.txt | tail 
 
 #Run reformat from bbmap suite
 /lustre/work/jmanthey/bbmap/reformat.sh in=${workdir}/01_blochDNA/${basename_array}_2_modoc.fastq.gz out1=${workdir}/01_blochDNA/${basename_array}_R1.fastq.gz out2=${workdir}/01_blochDNA/${basename_array}_R2.fastq.gz
+
+#Remove unneccesary fastq files
+cd /lustre/scratch/brameado/Blochmannia_Local_Adaptation/01_blochDNA
+rm *_R1.fastq.gz
+rm *_R2.fastq.gz
