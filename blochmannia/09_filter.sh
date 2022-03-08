4c1412482e2e039abda4fa83973814f8c17214ce
Bmeadows27 <55255460+Bmeadows27@users.noreply.github.com>
2022-03-08 16:18:05 -0600

    Update 09_filter.sh

--- blochmannia/09_filter.sh
+++ blochmannia/09_filter.sh
@@ -19,7 +19,7 @@ workdir=/lustre/scratch/brameado/Blochmannia_Local_Adaptation
 vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 10 --min-alleles 2 --max-alleles 2 --maf 0.05 --remove-indels --recode --recode-INFO-all --out ${workdir}/04_filter_vcf_admixture/${input_array}
 
 #run vcftools with SNP output for GEA tests
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 0.8 --minQ 20 --minGQ 20 --minDP 10 --min-alleles 2 --max-alleles 2 --maf 0.05 --remove-indels --recode --recode-INFO-all --out ${workdir}/06_filter_vcf_GEA/${input_array}
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 0.8 --minQ 20 --minGQ 20 --minDP 10 --min-alleles 2 --max-alleles 2 --maf 0.05 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_filter_vcf_GEA/${input_array}
 
 # run bcftools to simplify the vcftools output for ADMIXTURE
 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n ' ${workdir}/04_filter_vcf_admixture/${input_array}.recode.vcf > ${workdir}/04_filter_vcf_admixture/${input_array}.simple.vcf
