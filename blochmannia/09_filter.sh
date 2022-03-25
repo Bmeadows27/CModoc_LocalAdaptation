a917167d7902ae705896a2ed402775abe4dd8c12
Bmeadows27 <55255460+Bmeadows27@users.noreply.github.com>
2022-03-25 16:15:09 -0500

    Update 09_filter.sh

--- blochmannia/09_filter.sh
+++ blochmannia/09_filter.sh
@@ -16,10 +16,10 @@ input_array=$( head -n${SLURM_ARRAY_TASK_ID} helper6.txt | tail -n1 )
 workdir=/lustre/scratch/brameado/Blochmannia_Local_Adaptation
 
 # run vcftools with SNP output for ADMIXTURE 
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 10 --min-alleles 2 --max-alleles 2 --maf 0.05 --remove-indels --recode --recode-INFO-all --out ${workdir}/04_filter_vcf_admixture/${input_array}
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --remove-indv B015 --remove-indv B104 --remove-indv B246 --max-missing 1.0 --minQ 20 --minGQ 20 --minDP 10 --min-alleles 2 --max-alleles 2 --maf 0.05 --remove-indels --recode --recode-INFO-all --out ${workdir}/04_filter_vcf_admixture/${input_array}
 
 #run vcftools with SNP output for GEA tests
-vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --max-missing 0.8 --minQ 20 --minGQ 20 --minDP 10 --min-alleles 2 --max-alleles 2 --maf 0.05 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_filter_vcf_GEA/${input_array}
+vcftools --vcf ${workdir}/03_vcf/${input_array}.g.vcf  --remove-indv B015 --remove-indv B104 --remove-indv B246 --max-missing 0.8 --minQ 20 --minGQ 20 --minDP 10 --min-alleles 2 --max-alleles 2 --maf 0.05 --remove-indels --recode --recode-INFO-all --out ${workdir}/05_filter_vcf_GEA/${input_array}
 
 # run bcftools to simplify the vcftools output for ADMIXTURE
 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%GT]\n ' ${workdir}/04_filter_vcf_admixture/${input_array}.recode.vcf > ${workdir}/04_filter_vcf_admixture/${input_array}.simple.vcf
