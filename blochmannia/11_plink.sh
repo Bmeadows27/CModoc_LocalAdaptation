e0d7702d5a9c143f12a88355dadb94b6fb592d7e
Bmeadows27 <55255460+Bmeadows27@users.noreply.github.com>
2022-04-04 16:18:19 -0500

    Create 11_plink.sh

---
+++ blochmannia/11_plink.sh
@@ -0,0 +1,4 @@
+#Converting VCF to .bed file for FEEMS analysis
+cd /lustre/scratch/brameado/Blochmannia_Local_Adaptation/04_filter_vcf_admixture
+
+/home/jmanthey/plink --bloch_admixture --make-bed --out blochadmix
